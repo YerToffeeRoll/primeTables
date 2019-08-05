f79fabe8aaf3388a7547adc8debf5c03901f8eba
sam <sam_killen_@hotmail.com>
2019-08-05 18:10:21 +0100

    -Created different test cases to check the number given is primes -Created function to determine whether number given is prime via the Sieve of Eratosthenes

--- jasmine/spec/indexSpec.js
+++ jasmine/spec/indexSpec.js
@@ -1,8 +1,14 @@
 describe("Primes multiplcation table", function() {
   describe("isPrime", function () {
     it("determines if a given integer is prime", function () {
-      expect(isPrime(0)).toBe(true);
-
+      expect(isPrime(0)).toBe(false);
+      expect(isPrime(1)).toBe(false);
+      expect(isPrime(2)).toBe(true);
+      expect(isPrime(3)).toBe(true);
+      expect(isPrime(4)).toBe(false);
+      expect(isPrime(13)).toBe(true);
+      expect(isPrime(99)).toBe(false);
+      expect(isPrime(105943)).toBe(true);
     });
   });
 
--- jasmine/src/index.js
+++ jasmine/src/index.js
@@ -1,6 +1,19 @@
-function isPrime(n) {
-return true;
-}
 
+//Utilizes the sieve of Eratosthene to determine primality
+function isPrime(n) {
+    if (n <= 1) {
+        return false;
+    }
+    const numberSqrt = Math.floor(Math.sqrt(n));
+    let i = 2;
+    while (i <= numberSqrt) {
+        if (n % i === 0) {
+            return false;
+        }
+        i += 1;
+    }
+
+    return true;
+}
 
 
