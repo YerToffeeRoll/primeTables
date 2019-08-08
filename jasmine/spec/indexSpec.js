79fa4a874931261852c0b8e4700408c3c0e12883
sam <sam_killen_@hotmail.com>
2019-08-08 15:26:20 +0100

    -Created new jasmine test for the get primes table function -Added a new function getPrimes which creates a prime table using the sieve of Eratosthenes -Create a get started function and binded the input box to it -Added some functionality which prints the prime table out all nicey nicey

--- jasmine/spec/indexSpec.js
+++ jasmine/spec/indexSpec.js
@@ -13,6 +13,18 @@ describe("Primes multiplcation table", function() {
   });
 
 
+  describe("getPrimes", function () {
+    it("determines if a given integer can create a prime table", function () {
+      let test1 = [];
+      let test2 = [ 2 ];
+      let test3 = [2,3,5,7,11,13];
+      expect(getPrimes(0)).toEqual(test1);
+      expect(getPrimes(2)).toEqual(test2);
+      expect(getPrimes(13)).toEqual(test3);
+    });
+  });
+
+
 
 });
 
--- jasmine/src/index.js
+++ jasmine/src/index.js
@@ -1,4 +1,3 @@
-
 //Utilizes the sieve of Eratosthene to determine primality
 function isPrime(n) {
     if (n <= 1) {
@@ -14,6 +13,55 @@ function isPrime(n) {
     }
 
     return true;
+}
+
+//Utilizes the sieve of Eratosthene to create table
+function getPrimes(max) {
+    let sieve = [], i, j, primes = [];
+    for (i = 2; i <= max; ++i) {
+        if (!sieve[i]) {
+            // i has not been marked -- it is prime
+            primes.push(i);
+
+            for (j = i << 1; j <= max; j += i) {
+                sieve[j] = true;
+            }
+        }
+    }
+    return primes;
+}
+
+function getStarted() {
+
+    //basic initialising and  interfacing of variables
+    let text = document.getElementById('input1').value;
+    let tbl = document.createElement('table'),
+        tbd = tbl.appendChild(document.createElement('tbody')),
+        tr, td, i, found = 0;
+
+    if (isPrime(text)) {
+        //if input is a prime, then we can get the table
+        let primedata = getPrimes(text);
+
+        if (primedata) {
+            for (i in primedata) {
+                // if the number of found numbers is a multiple of 10, start a new row
+                // the first prime we find will be "number 0", which is divisible by 10.
+                if (found % 10 == 0) tr = tbd.appendChild(document.createElement('tr'));
+                td = tr.appendChild(document.createElement('td'));
+                td.appendChild(document.createTextNode(primedata[i]));
+                found++
+            }
+            if (found % 10 != 0) {
+                td = tr.appendChild(document.createElement('td'));
+                td.colSpan = 10 - found % 10;
+            }
+            document.body.appendChild(tbl);
+            tbl.setAttribute("border", 1);
+        }
+    } else {
+        document.getElementById('output').append('Not a prime number');
+    }
 }
 
 
